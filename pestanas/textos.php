c2d2fca01faae27c362a8d501b96db6376a41d4f
Luz <Luz@idoneoweb.es>
2025-11-06 19:59:57 +0100

    lectura 6/11 vista-3_messeger

--- pestanas/textos.php
+++ pestanas/textos.php
@@ -1,7 +1,7 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/trabajoFinal/php/login_seguridad/seguridad.php'; ?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/trabajoFinal/pestanas/php/loading_message.php'; ?>
+ <!-- <?php //include $_SERVER['DOCUMENT_ROOT'] . '/trabajoFinal/pestanas/php/loading_message.php'; ?> 
 <link rel="stylesheet" href="pestanas/css/loading_message.css">
-<script src="pestanas/js/loading_message.js"></script>
+<script src="pestanas/js/loading_message.js"></script> -->
 
 <div class="contenedor-textos">
     <div id="notificacion-flotante" class="notificacion-flotante">
